f65a33a156c1a960beb05304afd495958e3bc97c
RalapZ <zhuralap@gmail.com>
2021-05-09 15:46:12 +0800

    add code

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/RalapZ/dingtalkopenldap/model"
-	"github.com/RalapZ/dingtalkopenldap/service"
-	"strconv"
+	"log"
+	"time"
 )
 
 func main() {
@@ -12,21 +12,28 @@ func main() {
 	AppSecret := "pUTLevcbvk9EWdJKF7kzj5-Txc_7CHdyrO9eFiObLj7Qb6F3Y7q8YfXnwhtIFTvI"
 	method := "GET"
 	Url_token := "https://oapi.dingtalk.com/gettoken?appkey=" + AppKey + "&appsecret=" + AppSecret
-	service.GetToken(method, Url_token)
-	fmt.Println(model.Token)
+	model.GetToken(method, Url_token)
+	log.Println(model.Token)
 
 	//获取所有部门信息https://oapi.dingtalk.com/topapi/v2/department/listsub
-	UrlListSub := "https://oapi.dingtalk.com/department/list?access_token=" + model.Token
-	sub := service.GetListSub(method, UrlListSub)
-	fmt.Println(sub)
-
-	UrlDepDetail := "https://oapi.dingtalk.com/topapi/v2/department/get?access_token=" + model.Token + "&dept_id="
-	for _, k := range sub {
-		url := UrlDepDetail + strconv.Itoa(k.Id)
-		subdetail := service.GetSubDetailInfo(method, url)
-		fmt.Printf("%v\n", subdetail)
-		//fmt.Printf("%+#v\n",subdetail)
-	}
-
+	//UrlListSub := "https://oapi.dingtalk.com/department/list?access_token=" + model.Token
+	//sub := model.GetListSub(method, UrlListSub)
+	//log.Println(sub)
+	//
+	//UrlDepDetail := "https://oapi.dingtalk.com/topapi/v2/department/get?access_token=" + model.Token + "&dept_id="
+	//for _, k := range sub {
+	//	url := UrlDepDetail + strconv.Itoa(k.Id)
+	//	subdetail := model.GetSubDetailInfo("POST", url,)
+	//	log.Printf("%v\n", subdetail)
+	//	//fmt.Printf("%+#v\n",subdetail)
+	//}
+	UrlDepSubId := "https://oapi.dingtalk.com/topapi/v2/department/listsubid?access_token=" + model.Token
+	//model.GetListSubId("POST",1 , UrlDepSubId)
+	model.InitListSubId("POST",1 , UrlDepSubId)
 	//GetListSubId
+	time.Sleep(1000*time.Microsecond)
+	log.Println(model.DepListId)
+	for k,v:=range model.DepListDetailInfo{
+		fmt.Println(k,v)
+	}
 }
--- model/model.go
+++ model/model.go
@@ -1,8 +1,26 @@
 package model
 
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+)
+const (
+	UrlToken = "https://oapi.dingtalk.com/gettoken"                     //获取token信息接口
+	UrlDepDetail = "https://oapi.dingtalk.com/topapi/v2/department/get"   //获取部门详细信息接口
+)
+
 var (
-	Token  string
-	METHOD = "get"
+	Token             string                                    //接口token信息
+	METHOD                = "GET"                               //请求方法
+	DepListId             =make(map[int][]int)                  //department 子列表信息
+	DepListDetailInfo     =make(map[int]DepDetailInfo)          //department 详细信息
+	DepID             int =1                                    //默认department ID信息
 )
 
 type Tokenstr struct {
@@ -58,4 +76,121 @@ type ResponseDepListSubId struct {
 	} `json:"result"`
 	Errmsg    string `json:"errmsg"`
 	RequestId string `json:"request_id"`
+}
+
+
+//初始化组织信息的结构内存map信息,   待补充并发初始化数据
+func InitListSubId(method string,DepID int, url string) {
+	body:= make(map[string]interface{})
+	body["dept_id"]=DepID
+	str := UrlRequest(method, url,&body)
+	json_info := ResponseDepListSubId{}
+	err := json.Unmarshal(str, &json_info)
+	if err != nil {
+		log.Println(err)
+	}
+	DepListId[DepID]=json_info.Result.DeptIdList //更新部门子部门map信息
+
+	info, err := GetSubDetailInfo("POST", DepID)
+	if err!=nil{
+		log.Println(err)
+		//return
+	}
+	DepListDetailInfo[DepID] = info.Result       //更新部门部门详细信息到map
+	for _,v:=range json_info.Result.DeptIdList{
+		InitListSubId(method,v,url)
+	}
+}
+
+//获取列表信息
+func GetListSubId(method string,DepID int, url string) {
+	body:= make(map[string]interface{})
+	body["dept_id"]=DepID
+	str := UrlRequest(method, url,&body)
+	//body_json := err2
+	json_info := ResponseDepListSubId{}
+	err := json.Unmarshal(str, &json_info)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+
+
+func UrlRequest(method string, url string, body *map[string]interface{}) []byte {
+	C := &http.Client{}
+	//bodyinfo:=nil
+	var bodyinfo io.Reader=nil
+	if body == nil {
+		log.Println("body is null")
+	} else {
+		for k, v := range *body {
+			log.Println(k, v)
+		}
+		json_body,err:=json.Marshal(body)
+		if err != nil{
+			log.Println(err)
+		}else{
+			bodyinfo=bytes.NewReader(json_body)
+		}
+	}
+	resq, err := http.NewRequest(method, url, bodyinfo)
+	if err != nil {
+		log.Println(err)
+	}
+	//defer resq.Body.Close()
+
+	res, err := C.Do(resq)
+	defer res.Body.Close()
+	if err != nil {
+		log.Println(err)
+	}
+	str, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	return str
+}
+
+func GetToken(method string, url string) {
+	str := UrlRequest(method, url, nil)
+	json_info := Tokenstr{}
+	err := json.Unmarshal(str, &json_info)
+	if err != nil {
+		log.Println(err)
+	}
+	//token := json_info.Access_token
+	Token = json_info.Access_token
+	//return token
+}
+
+
+func GetListSub(method string, url string) []SubInfo {
+	str := UrlRequest(method, url, nil)
+	json_info := ListSubInfo{}
+	err := json.Unmarshal(str, &json_info)
+	if err != nil {
+		log.Println(err)
+		log.Println(err)
+	}
+	//token:=json_info.Access_token
+	//DepartmentInfo:=
+	return json_info.Department
+}
+
+func GetSubDetailInfo(method string,DepID int) (ResponseDepDetailInter,error) {
+	UrlDepDetail := "https://oapi.dingtalk.com/topapi/v2/department/get?access_token=" + Token + "&dept_id=" + strconv.Itoa(DepID)
+	str := UrlRequest(method, UrlDepDetail, nil)
+	json_info := ResponseDepDetailInter{}
+	err := json.Unmarshal(str, &json_info)
+	if err != nil {
+		return json_info,err
+	}
+	if json_info.Errcode!=0{
+		log.Println("getting department detail information is something wrong",DepID)
+		return json_info,errors.New("getting department detail information is something wrong"+strconv.Itoa(DepID))
+	}
+	//token:=json_info.Access_token
+	//DepartmentInfo:=
+	return json_info,nil
 }
